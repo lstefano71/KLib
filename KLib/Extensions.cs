8f908b151f403111f520cd3bc3f3aff397e74481
stf@apl.it <stf@apl.it>
2017-02-28 14:26:13 +0100

    Add project files.

---
+++ KLib/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace KLib.Extensions
+{
+	static class Extensions {
+		public static IEnumerable<T> Use<T>(this T obj) where T : IDisposable
+		{
+			try {
+				yield return obj;
+			} finally {
+				if (obj != null)
+					obj.Dispose();
+			}
+		}
+
+		public static IEnumerable<Tuple<Int32, X>> WithIndex<X>(this IEnumerable<X> lhs)
+		{
+			return lhs.WithIndex(0);
+		}
+
+		public static IEnumerable<Tuple<Int32, X>> WithIndex<X>(this IEnumerable<X> lhs, Int32 initial)
+		{
+			Int32 index = initial - 1;
+
+			return lhs.Select(x => new Tuple<Int32, X>(++index, x));
+		}
+
+		public static IEnumerable<string> FromReader(this TextReader input)
+		{
+			string s;
+			while((s=input.ReadLine()) != null) {
+				yield return s;
+			}
+		}
+	}
+}
---
+++ KLib/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KLib.Extensions;
+using Microsoft.Win32;
+
+namespace KLib
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Import();
+		}
+
+		static void Export()
+		{
+			string registryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+			using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registryKey)) {
+				var q = from a in key.GetSubKeyNames()
+								from r in key.OpenSubKey(a).Use()
+								let d = r.GetValue("DisplayName")
+								where d != null
+								select d.ToString();
+
+				foreach (var s in q.WithIndex()) {
+					Console.WriteLine("{0}: {1}",s.Item1,s.Item2);
+				}
+			}
+
+		}
+
+		static void Import()
+		{
+
+			var q = from l in Console.In.FromReader()
+							let s = l.Trim()
+							where !string.IsNullOrEmpty(s) && s[0] != '#'
+							select s;
+
+			foreach (var s in q.WithIndex()) {
+				Console.WriteLine("{0}: {1}", s.Item1, s.Item2);
+			}
+		}
+	}
+}
